3ce8b83e85ea0939f8d12fd0ae04b0a1b80de50d
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2020-08-24 19:11:47 -0300

    slider tabs added

---
+++ src/Components/RangeSlider/RangeSlider.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+
+import css from './RangeSlider.module.css';
+
+class RangeSlider extends Component {
+
+    constructor() {
+    super();
+        this.state = { 
+            val1: 0,
+            val2: 20000
+        }
+
+        this.slider1 = React.createRef();
+        this.slider2 = React.createRef();
+    }
+    
+    
+    updatePriceLabels = (e, slider) => { 
+
+    
+        let val1, val2;
+      
+        if (slider === 'one') {
+            val1 = e.target.value;
+            val2 = this.state.val2;    
+        } 
+        else if (slider === 'two') {
+            val1 = this.state.val1;
+            val2 = e.target.value;
+        }
+
+        val1 = parseInt(val1);
+        val2 = parseInt(val2);
+        
+        if (val1 >= val2) { 
+
+            console.log('val1 >= val2')
+
+            this.setState({
+                val1: val2 - 1000,
+                val2: val2
+            });
+            return;
+        } 
+        else if (val2 <= val1) { 
+
+            console.log('val2 <= val1')
+
+            this.setState({
+                val1: val1 + 1000,
+                val2: val1,
+            });
+            
+            return;
+        } 
+        else {
+            this.setState({
+                    val1: val1,
+                    val2: val2
+            });
+        }
+    }
+    
+    render() {
+        return (
+            <div className={css.PriceSlider}>
+
+                <p>{this.state.val1} - {this.state.val2}</p>
+                <input ref={this.slider1} 
+                    value={this.state.val1} 
+                    min="0" max="20000" step="1000" 
+                    type="range" 
+                    onChange={(e) => this.updatePriceLabels(e, 'one')}></input>
+                <input ref={this.slider2} 
+                    value={this.state.val2} 
+                    min="0" max="20000" step="1000" 
+                    type="range" 
+                    onChange={(e) => this.updatePriceLabels(e, 'two')}></input>
+                    
+            </div>
+        
+        )
+    }
+}
+    
+export default RangeSlider;
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -5,6 +5,8 @@ import css from './Search.module.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+import RangeSlider from '../RangeSlider/RangeSlider';
+
 
 class Search extends Component {
     constructor(props) {
@@ -12,6 +14,8 @@ class Search extends Component {
         this.state = { 
             dateDep: new Date(),
             dateRet: new Date(),
+            showReturn: false,
+
          }
     }
     render() { 
@@ -20,50 +24,97 @@ class Search extends Component {
             
 
             <div className={css.Tab}>
-                <button>One Way</button>
-                <button>Return</button>
+                <button onClick={() => this.setState({showReturn: false})}>One Way</button>
+                <button onClick={() => this.setState({showReturn: true})}>Return</button>
             </div>
 
-            <form className={css.FormSearch}>
-                <input type='text' placeholder='Enter Origin City'></input>
-                <input type='text' placeholder='Enter Destination City'></input>
+            {
+                this.state.showReturn
+                ?
+                <form className={css.FormSearchReturn}>
+                    <input type='text' placeholder='Enter Origin City'></input>
+                    <input type='text' placeholder='Enter Destination City'></input>
+                    
+
+                    <div className={css.FormDiv}>
+                        <label>Departure Date</label>
+                        <DatePicker
+                            selected={this.state.dateDep} 
+                            onChange={this.handleDateDepart}/>
+                    </div>
+
+                    <div className={css.FormDiv}>
+                        <label>Return Date</label>
+                        <DatePicker
+                            selected={this.state.dateRet} 
+                            onChange={this.handleDateReturn}/>
+                    </div>
                 
+                    <div className={css.FormDiv}>
+                        <label>Passengers</label>
+                        <select>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                            <option value='7'>7</option>
+                            <option value='8'>8</option>
+                            <option value='9'>9</option>
+                            <option value='10'>10</option>
+                        </select>
+                    </div>
 
-                <div className={css.FormDiv}>
-                    <label>Departure Date</label>
-                    <DatePicker
-                        selected={this.state.dateDep} 
-                        onChange={this.handleDateDepart}/>
-                </div>
+                    <div className={css.FormInput}>
+                        <input type='submit' value='Search'></input>
+                    </div>
+                    
+                </form>
+            :
+                <form className={css.FormSearch}>
 
-                <div className={css.FormDiv}>
-                    <label>Return Date</label>
-                    <DatePicker
-                        selected={this.state.dateRet} 
-                        onChange={this.handleDateReturn}/>
-                </div>
-               
-                <div className={css.FormDiv}>
-                    <label>Passengers</label>
-                    <select>
-                        <option value='1'>1</option>
-                        <option value='2'>2</option>
-                        <option value='3'>3</option>
-                        <option value='4'>4</option>
-                        <option value='5'>5</option>
-                        <option value='6'>6</option>
-                        <option value='7'>7</option>
-                        <option value='8'>8</option>
-                        <option value='9'>9</option>
-                        <option value='10'>10</option>
-                    </select>
-                </div>
+                    <input type='text' placeholder='Enter Origin City'></input>
+                    <input type='text' placeholder='Enter Destination City'></input>
+                    
 
-                <div className={css.FormInput}>
-                    <input type='submit' value='Search'></input>
-                </div>
+                    <div className={css.FormDiv}>
+                        <label>Departure Date</label>
+                        <DatePicker
+                            selected={this.state.dateDep} 
+                            onChange={this.handleDateDepart}/>
+                    </div>
                 
-            </form>
+                    <div className={css.FormDiv}>
+                        <label>Passengers</label>
+                        <select>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='6'>6</option>
+                            <option value='7'>7</option>
+                            <option value='8'>8</option>
+                            <option value='9'>9</option>
+                            <option value='10'>10</option>
+                        </select>
+                    </div>
+
+                    <div className={css.FormInput}>
+                        <input type='submit' value='Search'></input>
+                    </div>
+                    
+                </form>
+
+            }
+
+           
+
+            
+
+
+            <RangeSlider/>
 
         </div>
          );
